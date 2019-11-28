0c73906ba10dcd5f5e5b4c0126e044375da359fd
Kamil Pacanek <kamil.pacanek@gmail.com>
2019-11-28 22:16:15 +0100

    Updates

---
+++ precache-manifest.c55ce28fc48ad4d7d2a616d6eee7626a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e374f462ee405951ebab5f119fde36c",
+    "url": "/reactor-knockoff-enhanced/index.html"
+  },
+  {
+    "revision": "3874eb6b45d9300803d1",
+    "url": "/reactor-knockoff-enhanced/static/css/main.d1a5e895.chunk.css"
+  },
+  {
+    "revision": "fe6923a434b5a8932c98",
+    "url": "/reactor-knockoff-enhanced/static/js/2.a0cfeb20.chunk.js"
+  },
+  {
+    "revision": "3874eb6b45d9300803d1",
+    "url": "/reactor-knockoff-enhanced/static/js/main.18d94db1.chunk.js"
+  },
+  {
+    "revision": "f92b3c81cf8e8aeccfbd",
+    "url": "/reactor-knockoff-enhanced/static/js/runtime-main.63c447fc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactor-knockoff-enhanced/precache-manifest.52f00408ace9110a28ce86e708c7ffc2.js"
+  "/reactor-knockoff-enhanced/precache-manifest.c55ce28fc48ad4d7d2a616d6eee7626a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactor-knockoff-enhanced/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactor-knockoff-enhanced/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
